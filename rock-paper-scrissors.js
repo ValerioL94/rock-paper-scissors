c22847357c7365c589304a9240eb85bf878e8cd0
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-09-28 21:40:04 +0200

    Add functions to stop the loop after 5 wins/losses

--- rock-paper-scrissors.js
+++ rock-paper-scrissors.js
@@ -2,7 +2,7 @@ let roundResult = document.getElementById("roundResult");
 let playerScore = 0;
 let computerScore = 0;
 let gameResult = document.getElementById("gameResult");
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("#options button");
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -24,20 +24,30 @@ function playRound(playerSelection) {
     } else {
         roundResult.textContent = "It's a draw!";
     }
-
     document.getElementById("playerScore").textContent = `Player score: ${playerScore}`;
     document.getElementById("computerScore").textContent = `Computer score: ${computerScore}`;
 
+    endGame();
+}
+function endGame() {
     if (playerScore === 5) {
-        gameResult.textContent = "YOU WON THE GAME!";
+        gameResult.textContent = "YOU WON THE GAME! Reload to play again!";
+        gameResult.style.color = "Green";
+        buttonsOff();
     } else if (computerScore === 5) {
-        gameResult.textContent = "YOU LOST THE GAME!";
+        gameResult.textContent = "YOU LOST THE GAME! Reload to play again!";
+        gameResult.style.color = "Red";
+        buttonsOff();
     } else {
         gameResult.textContent = "";
     }
-
 }
 
+function buttonsOff() {
+    buttons.forEach((button) => {
+        button.disabled = true
+    })
+}
 
 buttons.forEach((button) => {
     button.addEventListener("click", function () {
